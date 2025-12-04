03b2ad1e83822937b166e3012e9b8cd9e9b3d15f
paucar-luis <tanirimachi@gmail.com>
2025-12-04 09:37:02 -0500

    cgmnf

---
+++ index.php
@@ -0,0 +1,175 @@
+<programas_estudio>
+    <disenio_programacion_web>
+        <nombre>DISEÑO Y PROGRAMACIÓN WEB</nombre>
+        <modulos>
+            <modulo1>
+                <nombre>ANALISIS Y DISEÑO DE SISTEMAS WEB</nombre>
+                <periodos>
+                    <p1>
+                        <nombre>I</nombre>
+                        <unidades_didacticas>
+                            <ud1>
+                                <nombre>ANALISIS Y DISEÑO ED SISTEMAS</nombre>
+                            </ud1>
+                            <ud2>
+                                <nombre>INTRODUCCIÓN DE BASES DE DATOS</nombre>
+                            </ud2>
+                            <ud3>
+                                <nombre>FUNDAMENTOS DE PROGRAMACION</nombre>
+                            </ud3>
+                            <ud4>
+                                <nombre>REDES E INTERNET</nombre>
+                            </ud4>
+                            <ud5>
+                                <nombre>ARQUITECTTURA DE COMPUTADORAS</nombre>
+                            </ud5>
+                            <ud6>
+                                <nombre>COMUNICACIÓN ORAL</nombre>
+                            </ud6>
+                            <ud7>
+                                <nombre>APLICACIONES EN INTERNET</nombre>
+                            </ud7>
+                        </unidades_didacticas>
+                    </p1>
+                    <p2>
+                        <nombre>II</nombre>
+                        <unidades_didacticas>
+                            <ud1>
+                                <nombre>INTERPRETACION Y PRODUCCION DE TEXTO</nombre>
+                            </ud1>
+                            <ud2>
+                                <nombre>OFIMATICA</nombre>
+                            </ud2>
+                            <ud3>
+                                <nombre>PROGRAMACION ORIENTADA A OBJETOS</nombre>
+                            </ud3>
+                            <ud4>
+                                <nombre>METODOLOGIA DE DESARROLLO DE SOFTWARE</nombre>
+                            </ud4>
+                            <ud5>
+                                <nombre>APLICACIONES SISTEMATIZADAS</nombre>
+                            </ud5>
+                            <ud6>
+                                <nombre>ARQUITECTURA DE SERVIDORES WEB</nombre>
+                            </ud6>
+                            <ud7>
+                                <nombre>TALLER DE BASE DE DATOS</nombre>
+                            </ud7>
+                    </unidades_didacticas>
+                    </p2>
+                </periodos>
+            </modulo1>
+            <modulo2>
+                <nombre>DESARROLLO DE APLIACIONES WEB</nombre>
+                <periodos>
+                    <p3>
+                        <nombre>III</nombre>
+                        <unidades_didacticas>
+                            <ud1>
+                                <nombre>ADMINISTRACION DE BASE DE DATOS</nombre>
+                            </ud1>
+                            <ud2>
+                                <nombre>PROGRAMACION DE APLICACIONES WEB</nombre>
+                            </ud2>
+                            <ud3>
+                                <nombre>DISEÑO DE INTERFACES WEB</nombre>
+                            </ud3>
+                            <ud4>
+                                <nombre>PRUEBA DE SOFTWARE</nombre>
+                            </ud4>
+                            <ud5>
+                                <nombre>INGLES  PARA LA COMUNICACION ORAL</nombre>
+                            </ud5>
+                        </unidades_didacticas>
+                    </p3>
+                    <p4>
+                        <nombre>IV</nombre>
+                        <unidades_didacticas>
+                            <ud1>
+                                <nombre>DESARROLLO DE ENTORNOS WEB</nombre>
+                            </ud1>
+                            <ud2>
+                                <nombre>PROGRAMACION DE SOLUCIONES WEB</nombre>
+                            </ud2>
+                            <ud3>
+                                <nombre>PROYECTOS DE SOFTWARE</nombre>
+                            </ud3>
+                            <ud4>
+                                <nombre>SEGURIDAD EN APLICACIONES WEB</nombre>
+                            </ud4>
+                            <ud5>
+                                <nombre>COMPRESIÓN Y REDACCION EN INGLES</nombre>
+                            </ud5>
+                            <ud6>
+                                <nombre>COMPORTAMIENTO ETICO</nombre>
+                            </ud6>
+                        </unidades_didacticas>
+                    </p4>
+                </periodos>
+            </modulo2>
+            <modulo3>
+                <nombre>DISEÑO DE SERVICIOS WEB</nombre>
+                <periodos>
+                    <p5>
+                        <nombre>V</nombre>
+                        <unidades_didacticas>
+                            <ud1>
+                                <nombre>PROGRAMACIONDE APLIACCIONES MOVILES</nombre>
+                            </ud1>
+                            <ud2>
+                                <nombre>MARKETING DIGITAL</nombre>
+                            </ud2>
+                            <ud3>
+                                <nombre>DISEÑO DE SOLUCIONES WEB</nombre>
+                            </ud3>
+                            <ud4>
+                                <nombre>GESTION Y ADMINISTRACION DE SITIOS WEB</nombre>
+                            </ud4>
+                            <ud5>
+                                <nombre>DIAGRAMACION DIGITAL</nombre>
+                            </ud5>
+                            <ud6>
+                                <nombre>SOLUCION DE PROBLEMAS</nombre>
+                            </ud6>
+                            <ud7>
+                                <nombre>OPORTUNIDADES DE NEGOCIOS</nombre>
+                            </ud7>
+                        </unidades_didacticas>
+                    </p5>
+                    <p6>
+                        <nombre>VI</nombre>
+                        <unidades_didacticas>
+                            <ud1>
+                                <nombre>PLATAFORMA DE SERCION WEB</nombre>
+                            </ud1>
+                            <ud2>
+                                <nombre>ILUSTRACION DE GRAFICOS DIGITALES</nombre>
+                            </ud2>
+                            <ud3>
+                                <nombre>ADMINISTRACION DE SERVICION WEB</nombre>
+                            </ud3>
+                            <ud4>
+                                <nombre>COMERCIO ELECTRONICO</nombre>
+                            </ud4>
+                            <ud5>
+                                <nombre>PLAN DE NEGOCIOS</nombre>
+                            </ud5>
+                        </unidades_didacticas>
+                    </p6>
+                </periodos>   
+            </modulo3>
+        </modulos>
+    </disenio_programacion_web>
+    <enfermeria_tecnica>
+
+    </enfermeria_tecnica>
+    <industrias_alimentos_bebidas>
+
+    </industrias_alimentos_bebidas>
+    <mecatronica_automotriz>
+
+    </mecatronica_automotriz>
+    <produccion_agropecuaria>
+
+    </produccion_agropecuaria>
+</programas_estudio>
---
+++ xmlv1.php
@@ -0,0 +1,158 @@
+<?php
+$ies = [];
+
+$udp1 = ['ANALISIS Y DISEÑO DE SISTEMAS',
+'INTRODUCCIÓN DE BASES DE DATOS',
+'FUNDAMENTOS DE PROGRAMACION', 
+'REDES E INTERNET',
+'ARQUITECTTURA DE COMPUTADORAS',
+'COMUNICACIÓN ORAL',
+'APLICACIONES EN INTERNET'];
+$udp2 = ['INTERPRETACION Y PRODUCCION DE TEXTO',
+'OFIMATICA','PROGRAMACION ORIENTADA A OBJETOS', 
+'METODOLOGIA DE DESARROLLO DE SOFTWARE',
+'APLICACIONES SISTEMATIZADAS',
+'ARQUITECTURA DE SERVIDORES WEB',
+'TALLER DE BASE DE DATOS'];
+$udp3 = ['ADMINISTRACION DE BASE DE DATOS',
+'PROGRAMACION DE APLICACIONES WEB',
+'DISEÑO DE APLICACIONES WEB',
+'GESTION DE PROYECTOS DE APLICACIONES WEB',
+'GESTION DE BASES DE DATOS',
+'GESTION DE SERVIDORES WEB',
+'GESTION DE APLICACIONES WEB'];
+$udp4 = ['PROGRAMACIONDE APLIACCIONES MOVILES',
+'PROGRAMACION DE APLICACIONES MOVILES',
+'GESTION DE PROYECTOS DE APLICACIONES MOVILES',
+'GESTION DE BASES DE DATOS','GESTION DE SERVIDORES MOVILES',
+'GESTION DE APLICACIONES MOVILES','GESTION DE APLICACIONES HIBRIDAS'];
+$udp5 = ['DISEÑO DE APLICACIONES DE ESCRITORIO',
+'PROGRAMACION DE APLICACIONES DE ESCRITORIO',
+'GESTION DE PROYECTOS DE APLICACIONES DE ESCRITORIO',
+'GESTION DE BASES DE DATOS',
+'GESTION DE SERVIDORES DE ESCRITORIO',
+'GESTION DE APLICACIONES DE ESCRITORIO',
+'GESTION DE APLICACIONES HIBRIDAS'];
+$ud6 = ['TALLER DE INTEGRACION I',
+'TALLER DE INTEGRACION II',
+'TALLER DE INTEGRACION III',
+'TALLER DE INTEGRACION IV',
+'TALLER DE INTEGRACION V',
+'TALLER DE INTEGRACION VI',
+'TALLER DE INTEGRACION VII'];
+
+
+//----------------------COMENTARIOS  -------//
+$p1 =[];
+$p1['nombre'] = 'I';
+$p1['unidades_didacticas'] = $udp1;
+ 
+$p2 =[];
+$p2['nombre'] = 'II';
+$p2['unidades_didacticas'] = $udp2;
+
+$p3 =[];
+$p3['nombre'] = 'III';
+$p3['unidades_didacticas'] = $udp3;
+
+$p4 =[];
+$p4['nombre'] = 'IV';
+$p4['unidades_didacticas'] = $udp4;
+
+$p5 =[];
+$p5['nombre'] = 'V';
+$p5['unidades_didacticas'] = $udp5;
+
+$p6 =[];
+$p6['nombre'] = 'VI';
+$p6['unidades_didacticas'] = $ud6;
+
+
+//----------------------------MODULOS  -------//
+$m1 = array();
+$m1['nombre'] = 'ANALISIS Y DISEÑO DE SISTEMAS WEB';
+$m1['periodos'] = array($p1, $p2);
+
+$m2 = array();
+$m2['nombre'] = 'DESARROLLO DE APLIACIONES WEB';
+$m2['periodos'] = array($p3, $p4);
+
+$m3 = array();
+$m3['nombre'] = 'DISEÑO DE SERVICIOS WEB';
+$m3['periodos'] = array($p5, $p6);
+
+//---------------------------PROGRAMAS DE ESTUDIO -------//
+$pe1 = array();
+$pe1['nombre'] = "DISEÑO Y PROGRAMACION WEB";
+$pe1['modulos'] = [$m1, $m2, $m3];
+
+ //-------------Enfermeria----
+$pe2 = array();
+$pe2['nombre'] = "Enfermería Técnica";
+$pe2['modulos'] = [];
+//----------- industrias-----
+$pe4 = array();
+$pe4['nombre'] = "Industrias de Alimentos y Bebidas";
+$pe4['modulos'] = [];
+ //----------mecatronica-----
+$pe3 = array();
+$pe3['nombre'] = "Mecatrónica Automotriz";
+$pe3['modulos'] = [];
+
+//-----------agropecuaria-----
+$pe5 = array();
+$pe5['nombre'] = "Producción Agropecuaria";
+$pe5['modulos'] = [];
+
+
+$ies['programas_de_estudio'] = "IES PÚBLICO HUANTA";
+$ies['programas_estudio'] = array($pe1, $pe2, $pe3, $pe4, $pe5);
+
+
+$xml = new DOMDocument('1.0', 'UTF-8');
+$xml->formatOutput = true;
+
+
+$et1 = $xml->createElement('ies');
+$xml->appendChild($et1);
+
+//------recorrer las carreras
+
+    $nombre_ies = $xml->createElement("nombre", $ies['nombre']);
+    $programas_ies = $xml->createElement("programas_estudio");
+    $et1->appendChild($nombre_ies);
+    $et1->appendChild($programas_ies);
+    foreach ($ies ["programas_estudio"] as $indice => $PEs) {
+        $num_pe = $xml->createElement("pe".$indice+1);
+        $nombre_pe = $xml->createElement("nombre", $PEs['nombre']);
+        foreach ($PEs['modulos'] as $indice_modulo=> $Modulo) {
+            $num_mod = $xml->createElement("mod".$indice_modulo+1);
+            $nom_mod = $xml->createElement("nombre", $Modulo['nombre']);
+            $uds = $xml->createElement("unidades_didacticas");
+            foreach ($Modulo['periodos'] as $indice_periodo => $Perido) {
+                $num_per = $xml->createElement("per".$indice_periodo+1);
+                $nom_per = $xml->createElement("nombre", $Periodo['nombre']);
+                foreach ($Periodo['unidades_didacticas']as $indice_ud => $Ud) {
+                    $num_ud = $xml->createElement("per".$indice_ud+1);
+                    $nom_ud = $xml->createElement("nombre", $Ud);
+                    $num_ud->appendChild($nom_ud);
+                    $uds->appendChild($num_ud);
+                }
+                $num_per->appendChild($nom_per);
+                $num_per->appendChild($uds);
+                $num_pe->appendChild($num_per);
+            }
+            $num_mod->appendChild($nom_mod);
+            $num_pe->appendChild($num_mod);
+        }
+        $num_pe->appendChild($nombre_pe);
+        $programas_ies->appendChild($num_pe);
+    }
+    $et1->appendChild($ies);
+
+
+//-------crear archivo para ponerle nombre
+$archivo = "ies.xml";
+$xml->save($archivo);
+
+?>
